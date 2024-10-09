4219b566c516cc77a92512e5db831c8b0c16a952
Suvineet Singh <47290250+SuvineetSingh@users.noreply.github.com>
2024-10-09 08:00:54 -0400

    Added dark-mode feature (#3)
    * Added dark-mode feature
    
    * Update DarkMode.js
    
    removed commented code
    
    ---------
    
    fixes #1 
    
    Co-authored-by: Suvineet Singh <suvineetsingh@suvineets-mbp.mynetworksettings.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BubbleWrap from "./components/BubbleWrap";
 import "./App.css";
+import DarkMode from "./components/DarkMode";
 
 function App() {
   return (
     <div className="App">
+      <DarkMode/>
       <h1>Virtual Bubble Wrap</h1>
       <BubbleWrap />
     </div>
